3bb4c79f766fc5f49b9d6da36d4e75b9c5489771
John Farrell <johnkevinfarrell93@gmail.com>
2021-03-12 15:05:59 +0000

    chaper 02 - arrays

---
+++ Chapter02/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] names;
+
+      names = new string[4];
+      names[0] = "John";
+      names[1] = "Ana";
+      names[2] = "Mikey";
+      names[3] = "Sandra";
+
+      for (int i = 0; i < names.Length; i++)
+      {
+        Console.WriteLine(names[i]);
+      }
+    }
+  }
+}
