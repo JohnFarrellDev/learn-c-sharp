ec91f3fd653e64879248122e9c21d8fb3ecf1c0a
John Farrell <johnkevinfarrell93@gmail.com>
2021-03-13 20:07:57 +0000

    chapter 03 - overflows

---
+++ Chapter03/Overflows/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Overflows
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = int.MaxValue - 1;
+      Console.WriteLine($"initial value is {x}");
+      x++;
+      Console.WriteLine($"after incrementing value is {x}");
+      x++;
+      Console.WriteLine($"after incrementing value is {x}");
+      x++;
+      Console.WriteLine($"after incrementing value is {x}");
+
+      checked
+      {
+        try
+        {
+          x = int.MaxValue - 1;
+          Console.WriteLine($"initial value is {x}");
+          x++;
+          Console.WriteLine($"after incrementing value is {x}");
+          x++;
+          Console.WriteLine($"after incrementing value is {x}");
+          x++;
+          Console.WriteLine($"after incrementing value is {x}");
+        }
+        catch (OverflowException exception)
+        {
+          Console.WriteLine($"Value overflowed but I caught the exception!");
+        }
+      }
+
+      unchecked
+      {
+        int y = int.MaxValue + 1;
+        Console.WriteLine($"Initial value of y is {y}");
+        y--;
+        Console.WriteLine($"After decrementing y is {y}");
+        y--;
+        Console.WriteLine($"After decrementing y is {y}");
+      }
+    }
+  }
+}
