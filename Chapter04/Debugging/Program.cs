394c94816f1d4617306cd0da3bbff2f70c66df7d
John Farrell <johnfarrell@fastmail.com>
2021-03-13 21:59:35 +0000

    chapter 04 - debugging

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Debugging
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double a = 4.5;
+      double b = 2.5;
+      Console.WriteLine($"{a} + {b} = {add(a, b)}");
+    }
+
+    static double add(double a, double b) => a + b;
+  }
+}
