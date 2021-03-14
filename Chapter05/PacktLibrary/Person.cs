1345635bbad479d06dbb0b87aca50a5e1cbc24f6
John Farrell <johnkevinfarrell93@gmail.com>
2021-03-14 21:07:00 +0000

    chapter 05 - oop

---
+++ Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared
+{
+  public partial class Person : System.Object
+  {
+    public string Name;
+    public DateTime DateOfBirth;
+    public WondersOfTheWorld FavoriteWonder;
+    public WondersOfTheWorld BucketList;
+    public List<Person> Children = new List<Person>();
+    // constant field so cannot change (better practice is read-only)
+    public const string Species = "Homo Sapien";
+
+    public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
+
+    public Person() : this("unknown", "Earth")
+    {
+      // set default values for fields including read-only
+      // Name = "unknown";
+      // Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName) : this(initialName, HomePlanet: "unknown") { }
+
+    public Person(string initialName, string HomePlanet)
+    {
+      Name = initialName;
+      this.HomePlanet = HomePlanet;
+      Instantiated = DateTime.Now;
+    }
+
+    public void WriteToConsole()
+    {
+      WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+    }
+
+    public string GetOrigin() => $"{Name} was born on {HomePlanet}";
+
+    public (string, int) GetFruit() => ("Apples", 5);
+
+    public (string Name, int Number) GetNamedFruit() => (Name: "Apples", Number: 5);
+
+    public static (string Name, int Number) GetPerson() => ("Anything", 42);
+
+    public string SayHello()
+    {
+      return $"{Name} says hello";
+    }
+
+    public string SayHello(string Name)
+    {
+      return $"{this.Name} says hello to {Name}";
+    }
+
+    // public string SayHelloTo(string Name)
+    // {
+    //   return $"{this.Name} says hello to {Name}";
+    // }
+
+    public String OptionalParameters(
+      string command = "Run!",
+      double number = 0.0d,
+      bool active = true
+    ) => $"command is {command}, number is {number}, active is {active}";
+
+    public void PassingParameters(int x, ref int y, out int z)
+    {
+      z = 99;
+
+      x++;
+      y++;
+      z++;
+    }
+  }
+
+  // if only storing up to 8 items can save space using byte type as storage instad of int
+  // ushort if 16, uint if 32, ulong if 64
+  [System.Flags]
+  public enum WondersOfTheWorld : byte
+  {
+    None = 0b_0000_0000,
+    Pyramids = 0b_0000_0001,
+    HangingGardens = 0b_0000_0010,
+    StatueOfZeus = 0b_0000_0100,
+    TempleOfArtemis = 0b_0000_1000,
+    MausoleumAtHalicarnassus = 0b_0001_0000,
+    ColossusOfRhodes = 0b_0010_0000,
+    LighthouseOfAlexandria = 0b_0100_0000
+  }
+}
---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person bob = new Person();
+      bob.Name = "bob";
+      bob.DateOfBirth = new DateTime(1972, 12, 25);
+      bob.FavoriteWonder = WondersOfTheWorld.HangingGardens;
+      bob.BucketList = WondersOfTheWorld.TempleOfArtemis | WondersOfTheWorld.ColossusOfRhodes;
+      bob.Children.Add(new Person() { Name = "Tom" });
+      bob.Children.Add(new Person() { Name = "Joe" });
+
+      var alice = new Person
+      {
+        Name = "Alice",
+        DateOfBirth = new DateTime(1984, 04, 12),
+        FavoriteWonder = WondersOfTheWorld.TempleOfArtemis
+      };
+
+      WriteLine(format: "{0}'s favourite wonder is {1}. Its integer is {2}.", arg0: bob.Name, arg1: bob.FavoriteWonder, arg2: (int)bob.FavoriteWonder);
+      WriteLine(format: "{0}'s bucket list is {1} or as an int {2}.", arg0: bob.Name, arg1: bob.BucketList, arg2: (int)bob.BucketList);
+
+      WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+      foreach (Person child in bob.Children)
+      {
+        WriteLine(child.Name);
+      }
+
+
+
+      BankAccount.InterestRate = 0.1M;
+      var JohnAccount = new BankAccount();
+
+      bob.WriteToConsole();
+      WriteLine(bob.GetOrigin());
+
+      (string, int) fruit = bob.GetFruit();
+      WriteLine($"{fruit.Item1}, {fruit.Item2} amount");
+
+      var fruitNamed = bob.GetNamedFruit();
+      WriteLine($"{fruitNamed.Name}, {fruitNamed.Number} amount");
+
+      var thing1 = ("Neville", 4);
+      WriteLine($"{thing1.Item1} has {thing1.Item2} children");
+
+      var thing2 = (bob.Name, bob.Children.Count);
+      WriteLine($"{thing2.Name} has {thing2.Count} children");
+
+      (string name, int age) tupleWithFields = Person.GetPerson();
+      // can destructure tuple into its fields on assignment
+      (string name, int age) = Person.GetPerson();
+
+      WriteLine(bob.SayHello());
+      WriteLine(bob.SayHello("Margie"));
+
+      WriteLine(bob.OptionalParameters());
+      WriteLine(bob.OptionalParameters(number: 3.14));
+      WriteLine(bob.OptionalParameters("drive", 3.14, false));
+
+      int a = 10;
+      int b = 20;
+      int c = 30;
+      WriteLine($"Before: A = {a}, B = {b}, C = {c}");
+      bob.PassingParameters(a, ref b, out c);
+      WriteLine($"After: A = {a}, B = {b}, C = {c}");
+
+      var sam = new Person()
+      {
+        Name = "Sam",
+        DateOfBirth = new DateTime(1965, 11, 05)
+      };
+      WriteLine(sam.Origin);
+      WriteLine(sam.Age);
+      WriteLine(sam.Greeting);
+
+      sam.FavouriteIceCream = "Chocolate";
+      WriteLine(sam.FavouriteIceCream);
+
+      sam.Children.Add(new Person("Beth"));
+      sam.Children.Add(new Person("Ana"));
+
+      WriteLine(sam[0].Name);
+      WriteLine(sam[1].Name);
+    }
+  }
+}
