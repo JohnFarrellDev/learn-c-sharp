d77e329d4228b5ad8e17c2cc8580cdecb1733bdc
John Farrell <johnkevinfarrell93@gmail.com>
2021-03-21 20:43:01 +0000

    chapter 05 - records

---
+++ Chapter05/PacktLibrary9/Records.cs
@@ -0,0 +1,15 @@
+namespace PacktLibrary9
+{
+  public class ImmutablePerson
+  {
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+  }
+
+  public record ImmutableVehicle
+  {
+    public int Wheels { get; init; }
+    public string Color { get; init; }
+    public string Brand { get; init; }
+  }
+}
